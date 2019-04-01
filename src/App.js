f9827f7281c138b3f090c7154288fcd74991b2f4
jia <dliou471@gmail.com>
2019-03-31 23:12:30 -0400

    jest enzyme counter app

--- src/App.js
+++ src/App.js
@@ -3,23 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state={
+      counter:0,
+      showError : 'none'
+    }
+
+    this.decrese = this.decrese.bind(this);
+  }
+
+  decrese(){
+    if(this.state.counter==0){
+      this.setState({...this.state,showError:"inline"})
+    }
+    else{
+      this.setState({...this.state,showError:"none",counter:this.state.counter - 1})
+    }
+  }
+
   render() {
+    const styles = {
+      display:this.state.showError,
+      color:'red'
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div data-test="component-App">
+      <h1 data-test="counter-display">The counter is {this.state.counter}</h1>
+      <h3 data-test="error-component" style={styles}>The counter can not go  below 0.</h3><br></br>
+        <button data-test="increment" onClick={()=>this.setState({counter:this.state.counter+1,showError:'none'})}>Increment</button>
+        <button data-test="decrement" onClick={this.decrese}>Decrement</button>
       </div>
     );
   }
