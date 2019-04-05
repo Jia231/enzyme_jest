badc8f9f5d8413e3f1608d5287f14ad8de3d41ab
jia <dliou471@gmail.com>
2019-04-05 01:02:58 -0400

    input component added

--- src/Input.js
+++ src/Input.js
@@ -3,16 +3,25 @@ import { connect } from 'react-redux'
 
 class Input extends Component {
     render() {
+        const content = this.props.success ? null : (
+            <form className="form-inline">
+                <input
+                    data-test="input-box"
+                    id="word-guess"
+                    type="text" />
+                <button data-test="submit-button" type="submit"></button>
+            </form>
+        )
         return (
-            <div>
-                <button />
+            <div data-test="component-input">
+                {content}
             </div>
         )
     }
 }
 
-const mapStateToProps = (state) => {
-    return {}
+const mapStateToProps = ({ success }) => {
+    return { success }
 }
 
 export default connect(mapStateToProps)(Input)
--- src/Input.test.js
+++ src/Input.test.js
@@ -1,27 +1,37 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import { findByAttr, storeFactory } from "../test/testUtil";
+import { findByTestAttr, storeFactory } from "../test/testUtil";
 import Input from './Input';
+import { exportAllDeclaration } from '@babel/types';
 
 
 const setup = (initialState = {}) => {
     const store = storeFactory(initialState)
     const wrapper = shallow(<Input store={store} />).dive()
+    return wrapper;
 }
 
 setup()
 
 describe('render', () => {
     describe('word has not been guessed', () => {
+        let wrapper;
+        beforeEach(() => {
+            const initialState = { success: false }
+            wrapper = setup(initialState)
+        })
         test('should render component without error', () => {
-
+            const component = findByTestAttr(wrapper, "component-input");
+            expect(component.length).toBe(1)
         })
         test('should render input box', () => {
-
+            const component = findByTestAttr(wrapper, "input-box");
+            expect(component.length).toBe(1)
         })
         test('should submit button', () => {
-
+            const component = findByTestAttr(wrapper, "submit-button");
+            expect(component.length).toBe(1)
         })
 
     })
